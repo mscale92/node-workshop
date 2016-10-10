698780647db1774c5d971303b182f1e2abd1cbcd
Mary Scale <scalemary@gmail.com>
2016-10-10 17:24:36 +0000

    finished hello world node program

---
+++ hello_world.js
@@ -0,0 +1,9 @@
+//1. Create a program that outputs a string to the console, then ten seconds later
+//it outputs another string
+
+console.log("hello world");
+
+setTimeout(function(){
+    console.log("hello world again!!");
+},10000);
+
