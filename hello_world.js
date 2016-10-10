1e6c6923b32527910c98b11a4567d723a3221f76
Mary Scale <scalemary@gmail.com>
2016-10-10 17:37:55 +0000

    Finished the recursive hello world program with setTimeout

--- hello_world.js
+++ hello_world.js
@@ -7,3 +7,15 @@ setTimeout(function(){
     console.log("hello world again!!");
 },10000);
 
+
+//2.Use setTimeout to recursively write a program that spits out
+//hello world every ten seconds forever! no whiles
+
+function overAndover(){setTimeout(function(){
+    console.log("hello world");
+    overAndover();
+},10000);
+};
+//classic recursion, a function calling upon itself. In this case, forever.
+overAndover();
+//have to call the function so that it will run infinitely
